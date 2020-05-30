b6aa47fd0e4236395facd61814736a0de968b84f
Michael Fellinger <michael.fellinger@iohk.io>
2020-05-30 23:37:52 +0200

    fix git commit

--- update.sh
+++ update.sh
@@ -20,8 +20,5 @@ done
 rm -rf maintainers/scripts
 
 git add lib maintainers .version COPYING
-git commit \
-  -c user.name="Michael Fellinger" \
-  -c user.email="github@manveru.dev" \
-  -m "Update for $(date -u +%Y-%m-%d)"
+git -c user.name="Michael Fellinger" -c user.email="github@manveru.dev" commit -m "Update for $(date -u +%Y-%m-%d)"
 git push
