a0ad0b4fa8a9d0fd52da174cc9c308d8f07c4165
Michael Fellinger <github@manveru.dev>
2020-08-25 00:58:06 +0000

    Update for 2020-08-25

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -472,6 +472,12 @@
     githubId = 858965;
     name = "Andrew Morsillo";
   };
+  andehen = {
+    email = "git@andehen.net";
+    github = "andehen";
+    githubId = 754494;
+    name = "Anders Asheim Hennum";
+  };
   andersk = {
     email = "andersk@mit.edu";
     github = "andersk";
@@ -3352,6 +3358,12 @@
     github = "mweinelt";
     githubId = 131599;
     name = "Martin Weinelt";
+  };
+  hh = {
+    email = "hh@m-labs.hk";
+    github = "HarryMakes";
+    githubId = 66358631;
+    name = "Harry Ho";
   };
   hhm = {
     email = "heehooman+nixpkgs@gmail.com";
@@ -9443,5 +9455,11 @@
     email = "farid.m.zakaria@gmail.com";
     github = "fzakaria";
     githubId = 605070;
+  };
+  yevhenshymotiuk = {
+    name = "Yevhen Shymotiuk";
+    email = "yevhenshymotiuk@gmail.com";
+    github = "yevhenshymotiuk";
+    githubId = 44244245;
   };
 }
