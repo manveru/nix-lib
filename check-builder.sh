d81d9411ade8f5cc6cfc08977a1f1ff6f64e49b1
Michael Fellinger <michael.fellinger@iohk.io>
2021-10-25 17:38:46 +0200

    fix for new nix

---
+++ check-builder.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo /nix/store/* > "${out:?}"
--- flake.nix
+++ flake.nix
@@ -1,7 +1,25 @@
 {
   description = "A flake for the nixpkgs lib";
 
-  edition = 201909;
-
-  outputs = { self }: { lib = import ./lib; };
+  outputs = { self }: {
+    lib = import ./lib;
+    checks = let
+      nixpkgs = getFlake
+        "path:/nix/store/q3ai08408xzhn9z7lziazmv23z7s935w-source?lastModified=1590755949&narHash=sha256-obN47E6A9PAtzGmBXigrf7vLWBGeuXDxYQW%2f+FTjotM=&rev=e7752db2fb6c5e05f1de2b275535dbde07d30090";
+      systems = [ "x86_64-linux" ];
+      inherit (builtins) getFlake listToAttrs map;
+    in listToAttrs (map (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        name = system;
+        value = {
+          all = derivation {
+            name = "check";
+            system = "x86_64-linux";
+            builder = "/bin/sh";
+            args = [ ./check-builder.sh ];
+          };
+        };
+      }) systems);
+  };
 }
